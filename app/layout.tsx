fb226c7453db9482b7cf62020525e610954d49b5
Dayz <story759@gmail.com>
2026-01-22 14:56:04 +0900

    docs: add adsense verification script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Script from "next/script"; // Next.js 스크립트 컴포넌트 임포트
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,15 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <head>
-        {/* 구글 애드센스 소유권 확인 스크립트 */}
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6062349022770025"
-          crossOrigin="anonymous"
-          strategy="afterInteractive" // 페이지 상호작용 후 로드하여 성능 최적화
-        />
-      </head>
+      {/* head 태그를 명시하지 않아도 Script 컴포넌트가 알아서 처리합니다 */}
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6062349022770025"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
